364686eacf42be6f669366b337ce6c7582f37477
Jesse <jesse.lesnussa@gmail.com>
2019-07-22 23:02:03 +0200

    lay-out

--- run.py
+++ run.py
@@ -25,7 +25,7 @@ def upload_file(loading="loading 123"):
         medewerkers_df = pd.read_excel(medewerkers, index=False)
         medewerkers_df = medewerkers_df.iloc[1:]
         medewerkers_df = medewerkers_df.filter(['SollicitantID', 'naam', 'veld0', 'veld1', 'veld2'])
-        #medewerkers_df.to_sql('NoCore_Medewerkers', con=engine, if_exists='replace', index=False)
+        medewerkers_df.to_sql('NoCore_Medewerkers', con=engine, if_exists='replace', index=False)
 
         projecturen = request.files['projecturen']
         projecturen_df = pd.read_excel(projecturen)
@@ -34,16 +34,16 @@ def upload_file(loading="loading 123"):
         projecturen_df.columns.name = None
         projecturen_df=projecturen_df.rename(columns = {'Text187':'Tarief'})
         projecturen_df=projecturen_df.rename(columns = {'Refnr Klant':'RefnrKlant'})
-        #projecturen_df.to_sql('ProjectUrenTarief', con=engine, if_exists='replace', index=False)
+        projecturen_df.to_sql('ProjectUrenTarief', con=engine, if_exists='replace', index=False)
         
         jobs = request.files['jobs']
         jobs_df = pd.read_excel(jobs, index=False)
         empty_indexes = [9,10,11,12]
         jobs_df = jobs_df.drop(jobs_df.columns[empty_indexes], axis=1)
         jobs_df.columns.name = None
-        #jobs_df.to_sql('Temp_JobsUitPL', con=engine, if_exists='replace', index=False)
+        jobs_df.to_sql('Temp_JobsUitPL', con=engine, if_exists='replace', index=False)
 
-        return pd.read_sql_table('Temp_JobsUitPL').to_html(index=False)
+        return jobs_df.head(10).to_html(index=False)
     return render_template('index.html')
 
 
