956654ab4dffdca4d8711b1aed4ad4137ef8444c
Jesse <jesse.lesnussa@gmail.com>
2019-08-25 19:59:28 +0200

    updated python script

--- application.py
+++ application.py
@@ -1,3 +1,4 @@
+
 from flask import Flask, request, jsonify, render_template, send_file
 from sqlalchemy import create_engine
 try:
@@ -17,9 +18,11 @@ engine = create_engine("mssql+pyodbc:///?odbc_connect=%s" % params)
 loading = "test"
 print(engine)
 app=Flask(__name__)
-filename = ""
-for driver in pyodbc.drivers():
-    print(driver)
+
+
+def setColumnNames(df, columnNames):
+    df.columns = columnNames
+    return df
 
 @app.route("/")
 def homepage():
@@ -31,30 +34,28 @@ def upload_file():
     if request.method == 'POST':
         filename = "BuyFlex Dataformat export.xlsx" 
         connection = engine.connect()
-        #print(request.files['medewerkers'])
-        #print('Verwerken van medewerkers...')
-        #medewerkers = request.files['medewerkers']
-        #medewerkers_df = pd.read_excel(medewerkers, index=False)
-        #medewerkers_df = medewerkers_df.iloc[1:]
-        #medewerkers_df = medewerkers_df.filter(['SollicitantID', 'naam', 'veld0', 'veld1', 'veld2'])
-        #medewerkers_df.to_sql('NoCore_Medewerkers', con=engine, if_exists='replace', index=False)
-        #print('Succes!')
+        print(request.files['medewerkers'])
+        print('Verwerken van medewerkers...')
+        medewerkers = request.files['medewerkers']
+        medewerkers_df = pd.read_excel(medewerkers, index=False)
+        print(medewerkers_df)
+        medewerkers_df = setColumnNames(medewerkers_df, ['SollicitantID', 'naam', 'veld0', 'veld1', 'veld2'])
+        print(medewerkers_df)
+        medewerkers_df.to_sql('NoCore_Medewerkers', con=engine, if_exists='replace', index=False)
+
+        print('Succes!')
         print(request.files['projecturen'])
         print('Verwerken van projecturen...')
         projecturen = request.files['projecturen']
         projecturen_df = pd.read_excel(projecturen)
-        projecturen_df.columns = projecturen_df.iloc[0]
-        projecturen_df =  projecturen_df.iloc[1:]
-        projecturen_df=projecturen_df.rename(columns = {'Text187':'Tarief'})
-        projecturen_df=projecturen_df.rename(columns = {'Refnr Klant':'RefnrKlant'})
+        projecturen_df = projecturen_df.iloc[1:]
+        projecturen_df = setColumnNames(projecturen_df, ['SollicitantID', 'cNaam', 'JaarWeek', 'Tarief', 'Activiteitnummer', 'ActiviteitOmschr', 'RefnrKlant', 'Werkuren', 'UrenI', 'UrenII', 'UrenIII', 'UrenIV', 'UrenV', 'Verliesuren', 'Reis', 'Dagen', 'svwdagen' ])         
         print("converting string sollicitant id")
-        
-        for col in projecturen_df.columns: 
-            print(col) 
-        print(projecturen_df.head(5).to_string())
         projecturen_df['SollicitantID'] = projecturen_df['SollicitantID'].astype(str)
         print("success!")
+        print(projecturen_df)
         projecturen_df.to_sql('ProjectUrenTarief', con=engine, if_exists='replace', index=False)
+        
         print('Succes!')
         print(request.files['jobs'])
         print('Verwerken van jobs...')
@@ -63,9 +64,11 @@ def upload_file():
         empty_indexes = [9,10,11,12]
         print('Uitvoeren van ETL-proces')
         jobs_df = jobs_df.drop(jobs_df.columns[empty_indexes], axis=1)
-        jobs_df.columns.name = None
+        jobs_df = jobs_df.iloc[8:-1]
+        jobs_df = setColumnNames(jobs_df, ['ProjectId', 'MedewerkerId', 'NaamMedewerker', 'Job', 'BeginDatum', 'EindDatum', 'Tarief', 'Uurloon', 'Facturering'])
+        print(jobs_df)
         jobs_df.to_sql('Temp_JobsUitPL', con=engine, if_exists='replace', index=False)
-        print('Bestanden zijn ingevoerd in s..')
+        print('Bestanden zijn ingevoerd in staging, ETL-proces wordt gestart...')
         connection.execute(text("EXEC spGetDataFormat").execution_options(autocommit=True))
         print('Succes!')
         connection.close()
@@ -81,6 +84,6 @@ def download(filename):
     path = 'uploads/' + filename
     print(path)
     return send_file(path, as_attachment=True)
-
+    
 if __name__ == "__main__":
     app.run()
