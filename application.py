0378cc1f97d30a436cd8a0826fb604835ab8cd3b
Jesse <jesse.lesnussa@gmail.com>
2019-08-25 21:20:36 +0200

    Update application.py

--- application.py
+++ application.py
@@ -25,36 +25,37 @@ def setColumnNames(df, columnNames):
 def homepage():
     return render_template('index.html')
 
-@app.route("/upload", methods=['GET', 'POST'])
+@app.route("/upload")
 def upload_file():
-    if request.method == 'POST':
-        filename = "BuyFlex Dataformat export.xlsx" 
-        connection = engine.connect()
-        medewerkers = request.files['medewerkers']
-        medewerkers_df = pd.read_excel(medewerkers, index=False)
-        medewerkers_df = setColumnNames(medewerkers_df, ['SollicitantID', 'naam', 'veld0', 'veld1', 'veld2'])
-        medewerkers_df.to_sql('NoCore_Medewerkers', con=engine, if_exists='replace', index=False)
+   #
+   #if request.method == 'POST':
+   #filename = "BuyFlex Dataformat export.xlsx" 
+   #connection = engine.connect()
+   #medewerkers = request.files['medewerkers']
+   #medewerkers_df = pd.read_excel(medewerkers, index=False)
+   #medewerkers_df = setColumnNames(medewerkers_df, ['SollicitantID', 'naam', 'veld0', 'veld1', 'veld2'])
+   #medewerkers_df.to_sql('NoCore_Medewerkers', con=engine, if_exists='replace', index=False)
 
-        projecturen = request.files['projecturen']
-        projecturen_df = pd.read_excel(projecturen)
-        projecturen_df = projecturen_df.iloc[1:]
-        projecturen_df = setColumnNames(projecturen_df, ['SollicitantID', 'cNaam', 'JaarWeek', 'Tarief', 'Activiteitnummer', 'ActiviteitOmschr', 'RefnrKlant', 'Werkuren', 'UrenI', 'UrenII', 'UrenIII', 'UrenIV', 'UrenV', 'Verliesuren', 'Reis', 'Dagen', 'svwdagen' ])         
-        projecturen_df['SollicitantID'] = projecturen_df['SollicitantID'].astype(str)
-        projecturen_df.to_sql('ProjectUrenTarief', con=engine, if_exists='replace', index=False)
-        
-        jobs = request.files['jobs']
-        jobs_df = pd.read_excel(jobs, index=False)
-        empty_indexes = [9,10,11,12]
-        jobs_df = jobs_df.drop(jobs_df.columns[empty_indexes], axis=1)
-        jobs_df = jobs_df.iloc[8:-1]
-        jobs_df = setColumnNames(jobs_df, ['ProjectId', 'MedewerkerId', 'NaamMedewerker', 'Job', 'BeginDatum', 'EindDatum', 'Tarief', 'Uurloon', 'Facturering'])
-        jobs_df.to_sql('Temp_JobsUitPL', con=engine, if_exists='replace', index=False)
-        connection.execute(text("EXEC spGetDataFormat").execution_options(autocommit=True))
-        connection.close()
-        df =  pd.read_sql_table('vwDataFormat', con=engine)
-        df.to_excel( 'uploads/' + filename , index=False, header=False )         
+   #projecturen = request.files['projecturen']
+   #projecturen_df = pd.read_excel(projecturen)
+   #projecturen_df = projecturen_df.iloc[1:]
+   #projecturen_df = setColumnNames(projecturen_df, ['SollicitantID', 'cNaam', 'JaarWeek', 'Tarief', 'Activiteitnummer', 'ActiviteitOmschr', 'RefnrKlant', 'Werkuren', 'UrenI', 'UrenII', 'UrenIII', 'UrenIV', 'UrenV', 'Verliesuren', 'Reis', 'Dagen', 'svwdagen' ])         
+   #projecturen_df['SollicitantID'] = projecturen_df['SollicitantID'].astype(str)
+   #projecturen_df.to_sql('ProjectUrenTarief', con=engine, if_exists='replace', index=False)
+   #
+   #jobs = request.files['jobs']
+   #jobs_df = pd.read_excel(jobs, index=False)
+   #empty_indexes = [9,10,11,12]
+   #jobs_df = jobs_df.drop(jobs_df.columns[empty_indexes], axis=1)
+   #jobs_df = jobs_df.iloc[8:-1]
+   #jobs_df = setColumnNames(jobs_df, ['ProjectId', 'MedewerkerId', 'NaamMedewerker', 'Job', 'BeginDatum', 'EindDatum', 'Tarief', 'Uurloon', 'Facturering'])
+   #jobs_df.to_sql('Temp_JobsUitPL', con=engine, if_exists='replace', index=False)
+   #connection.execute(text("EXEC spGetDataFormat").execution_options(autocommit=True))
+   #connection.close()
+   #df =  pd.read_sql_table('vwDataFormat', con=engine)
+   #df.to_excel( 'uploads/' + filename , index=False, header=False )         
 
-        return render_template('export.html', value= df.to_html(index=False, header=False), filename=filename)
+   #return render_template('export.html', value= df.to_html(index=False, header=False), filename=filename)
     return render_template('index.html')
 
 
